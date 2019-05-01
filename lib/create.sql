c0148ca139a838c7642f1ecc8bdbea40430546f5
Marie Berry (Leugermann) <marie@creativeos.io>
2019-05-01 09:45:54 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,21 +1,21 @@
 CREATE TABLE projects (
-id INTEGER PRIMARY KEY,
-title TEXT,
-category TEXT,
-funding goal INTEGER,
-start_date TEXT,
-end_date TEXT,
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
 );
 
 CREATE TABLE users (
-id INTEGER PRIMARY KEY,
-name TEXT,
-age INTEGER
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
 );
 
 CREATE TABLE pledges (
-id INTEGER PRIMARY KEY,
-amount INTEGER,
-user_id INTEGER,
-project_id INTEGER
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
 );
